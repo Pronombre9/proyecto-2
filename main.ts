a07f4aaa5c4b3ab1503ba09acea2eb9996a86efa
Pronombre9 <96122451+Pronombre9@users.noreply.github.com>
2021-12-21 13:49:16 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,23 @@
 basic.forever(function () {
-	
+    if (input.buttonIsPressed(Button.A) && input.lightLevel() < 100) {
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    } else if (input.buttonIsPressed(Button.B) || input.lightLevel() > 100) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (false) {
+    	
+    } else {
+    	
+    }
 })
